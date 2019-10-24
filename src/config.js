8aa72f3ef43ce67973a9695fb6052f97d188a891
theahmdh <54501145+theahmdh@users.noreply.github.com>
2019-10-24 17:57:47 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,6 +9,9 @@ const myConfig = {
     operators:['lessThanInclusive','lessThan','equal','greaterThan','greaterThanInclusive'],
     messages:['Customer reached max allowed number of simultaneous promos', 'Inactive customer found!', 'Customer Reached Max Allowed Promos Cost'],
     categories: ['Customer Retention', 'Fair usage', 'Category 3', 'Category 4']
+  },
+  New:{
+	  a: ''
   }
 };
 
